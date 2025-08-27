a484ed644588805eb01afb273eade0e6e633751d
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2025-08-26 21:29:44 -0300

    add: `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
+import database from "../../../../infra/database.js"
+
 async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log(result.rows)
   response.status(200).json({ chave: "São Paulo rebaixado em 1990" });
 }
 
