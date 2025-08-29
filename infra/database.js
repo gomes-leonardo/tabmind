4622695c0e08c977a7e77684ea5e08b4bdb6b51d
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2025-08-29 17:34:54 -0300

    chore: make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -9,12 +9,17 @@ async function query(queryObject){
         database: process.env.POSTGRES_DB,
         password: process.env.POSTGRES_PASSWORD
     });
-    await client.connect();
 
-    const result = await client.query(queryObject);
+    try {
+        await client.connect();
+        const result = await client.query(queryObject);
+        return result;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.end();
+    }
 
-    await client.end();
-    return result;
 }
 
 export default {
