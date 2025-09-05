9cc442016e438527525ee9fe6e57590c781eea17
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2025-09-04 21:37:00 -0300

    adds: /api/v1/migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,32 +1,39 @@
-import database from "infra/database.js"
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
 
-async function status(request, response) {
-  const updatedAt = new Date().toISOString();
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
 
-  const databaseName = process.env.POSTGRES_DB;
-  const databaseVersionResult = await database.query('SHOW server_version;');
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
 
-  const databaseMaxConnectionsResult = await database.query('SHOW max_connections;');
-  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+    });
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
 
-  const databaseOpenedConnectionsResult = await database.query({
-    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName]
-  });
-  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
 
-  response.status(200).json(
-    { updated_at: updatedAt,
-      dependencies: {
-       database: {
-        version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionsValue),
-        opened_connections: databaseOpenedConnectionsValue
-       }
-      },
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
     }
-  );
-}
+    return response.status(200).json(migratedMigrations);
+  }
 
-export default status;
+  return response.status(405).end();
+}
