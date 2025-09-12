05c627fcf47f48b00c41d4aae8e2f4c1da8a7cfd
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2025-09-12 17:05:40 -0300

    add: eslint and eslint configs and workflow

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import pluginJest from "eslint-plugin-jest";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: [".next/**", "node_modules/**"],
+
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    files: ["**/*.spec.js", "**/*.test.js"],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
+    },
+  },
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+]);
