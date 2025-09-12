5bfbfe5f1c1f9962a1c4a6c68b6d71ae4b299a22
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2025-09-12 18:12:39 -0300

    feat: add husky and lint-staged pre commits validation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig([
     },
     rules: {
       "no-unused-vars": [
-        "warn",
+        "error",
         { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
     },
