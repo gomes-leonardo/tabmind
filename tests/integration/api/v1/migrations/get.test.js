1f88fde5c60661245378df3ddbccf0ad448c3442
gomes-leonardo <leonardo.r.gomes@hotmail.com>
2025-10-04 15:27:08 -0300

    test: correct describe in `migrations` test

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,11 +1,11 @@
-import orchestrator from "tests/orchestrator";
+import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await orchestrator.clearDatabase();
 });
 
-describe("POST /api/v1/migrations", () => {
+describe("GET /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     test("Retrieving pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
