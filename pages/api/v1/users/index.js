d881bba24f9078560906f83072bdaaf67fd57191
gomes-leonardo <leonardo.r.gomes@hotmail.com>
2025-10-15 07:27:51 -0300

    feat: create `user` model and `api/v1/users` endpoint

---
+++ pages/api/v1/users/index.js
@@ -0,0 +1,15 @@
+import { createRouter } from "next-connect";
+import user from "models/user";
+import controller from "infra/controller";
+
+const router = createRouter();
+
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function postHandler(request, response) {
+  const userInputValues = request.body;
+  const newUser = await user.create(userInputValues);
+  return response.status(201).json(newUser);
+}
