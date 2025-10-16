eaa04ce0c49867788a44f6eadfd8a039a5f1d37a
gomes-leonardo <leonardo.r.gomes@hotmail.com>
2025-10-16 15:48:31 -0300

    feat: create `user.findOneByUsername` method an `api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,16 +1,46 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+    SELECT 
+      *
+    FROM 
+      users
+    WHERE
+      LOWER(username) = LOWER($1)
+    LIMIT
+      1
+    ;`,
+      values: [username],
+    });
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O Username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
 
 async function create(userInputValues) {
-  await validateInputEmail(userInputValues.email);
-  await validateInputUsername(userInputValues.username);
+  await validateUniqueEmail(userInputValues.email);
+  await validateUniqueUsername(userInputValues.username);
 
   const newUser = runInsertQuery(userInputValues);
   return newUser;
+}
 
-  async function runInsertQuery(userInputValues) {
-    const result = await database.query({
-      text: `
+async function runInsertQuery(userInputValues) {
+  const result = await database.query({
+    text: `
     INSERT INTO users 
       (username, email, password) 
     VALUES 
@@ -18,18 +48,18 @@ async function create(userInputValues) {
     RETURNING
       *
     ;`,
-      values: [
-        userInputValues.username,
-        userInputValues.email,
-        userInputValues.password,
-      ],
-    });
-    return result.rows[0];
-  }
+    values: [
+      userInputValues.username,
+      userInputValues.email,
+      userInputValues.password,
+    ],
+  });
+  return result.rows[0];
+}
 
-  async function validateInputEmail(email) {
-    const result = await database.query({
-      text: `
+async function validateUniqueEmail(email) {
+  const result = await database.query({
+    text: `
     SELECT 
       email
     FROM 
@@ -37,18 +67,19 @@ async function create(userInputValues) {
     WHERE
       LOWER(email) = LOWER($1)
     ;`,
-      values: [email],
+    values: [email],
+  });
+  if (result.rowCount > 0) {
+    throw new ValidationError({
+      message: "O e-mail informado já está sendo utilizado.",
+      action: "Utilize outro e-mail para realizar o cadastro.",
     });
-    if (result.rowCount > 0) {
-      throw new ValidationError({
-        message: "O e-mail informado já está sendo utilizado.",
-        action: "Utilize outro e-mail para realizar o cadastro.",
-      });
-    }
   }
-  async function validateInputUsername(username) {
-    const result = await database.query({
-      text: `
+}
+
+async function validateUniqueUsername(username) {
+  const result = await database.query({
+    text: `
     SELECT 
       username
     FROM 
@@ -56,19 +87,19 @@ async function create(userInputValues) {
     WHERE
       LOWER(username) = LOWER($1)
     ;`,
-      values: [username],
+    values: [username],
+  });
+  if (result.rowCount > 0) {
+    throw new ValidationError({
+      message: "O username informado já está sendo utilizado.",
+      action: "Utilize outro username para realizar o cadastro.",
     });
-    if (result.rowCount > 0) {
-      throw new ValidationError({
-        message: "O username informado já está sendo utilizado.",
-        action: "Utilize outro username para realizar o cadastro.",
-      });
-    }
   }
 }
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
