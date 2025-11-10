dfde6ff9f188386beb384b1511e462963e7c4af8
ViveoPortais <viveo.portais@gmail.com>
2025-11-10 14:56:25 -0300

    refactor: reorganize `findOneByUsername` method

--- models/user.js
+++ models/user.js
@@ -2,35 +2,6 @@ import database from "infra/database";
 import { ValidationError, NotFoundError } from "infra/errors.js";
 import password from "./password";
 
-async function findOneByUsername(username) {
-  const userFound = await runSelectQuery(username);
-  return userFound;
-
-  async function runSelectQuery(username) {
-    const result = await database.query({
-      text: `
-    SELECT 
-      *
-    FROM 
-      users
-    WHERE
-      LOWER(username) = LOWER($1)
-    LIMIT
-      1
-    ;`,
-      values: [username],
-    });
-    if (result.rowCount === 0) {
-      throw new NotFoundError({
-        message: "O Username informado não foi encontrado no sistema.",
-        action: "Verifique se o username está digitado corretamente.",
-      });
-    }
-
-    return result.rows[0];
-  }
-}
-
 async function create(userInputValues) {
   await validateUniqueUsername(userInputValues.username);
   await hashPasswordInObject(userInputValues);
@@ -86,6 +57,64 @@ async function update(username, userInputValues) {
         userWithNewValues.password,
       ],
     });
+    return result.rows[0];
+  }
+}
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+    SELECT 
+      *
+    FROM 
+      users
+    WHERE
+      LOWER(username) = LOWER($1)
+    LIMIT
+      1
+    ;`,
+      values: [username],
+    });
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O Username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
+
+async function findOneByEmail(email) {
+  const userFound = await runSelectQuery(email);
+  return userFound;
+
+  async function runSelectQuery(email) {
+    const result = await database.query({
+      text: `
+    SELECT 
+      *
+    FROM 
+      users
+    WHERE
+      LOWER(email) = LOWER($1)
+    LIMIT
+      1
+    ;`,
+      values: [email],
+    });
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O email informado não foi encontrado no sistema.",
+        action: "Verifique se o email está digitado corretamente.",
+      });
+    }
+
     return result.rows[0];
   }
 }
@@ -156,8 +185,9 @@ async function validateUniqueEmail(email) {
 
 const user = {
   create,
-  findOneByUsername,
   update,
+  findOneByUsername,
+  findOneByEmail,
 };
 
 export default user;
